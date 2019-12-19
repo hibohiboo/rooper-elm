08433dbe6cb4c46592d6c2fa78c3d16b2af420e6
hibohiboo <swdice66@gmail.com>
2019-12-20 00:06:08 +0900

    eslint update. api 分割

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -4,7 +4,7 @@ import firebaseBackEnd from './firebase/FireBaseBackEnd';
 import Room, { addRoom, readRooms } from './firebase/Room';
 import { Elm } from './elm/Main'; //  eslint-disable-line import/no-unresolved
 import { hideLoader } from './utils/spinner';
-
+import { historyInit } from './utils/history';
 require('../css/styles.scss'); // tslint:disable-line no-var-requires
 
 
@@ -57,35 +57,10 @@ const initApp = async () => {
   //     },
   //   });
   // });
+  historyInit(ports);
 
   // spinnerを消す
   hideLoader();
-
-  // histroy api 設定.遷移 を防ぐ。aタグは増減するので、親でバブリングしたイベントを受ける。
-  //  eslint-disable-next-line no-unused-expressions
-  document.querySelector('.rooper-container')?.addEventListener('click', (event) => {
-    // aタグ以外は処理をしない
-    const { target }: { target: any } = event;
-    if (target && target.tagName && target.tagName.toUpperCase !== 'A') {
-      return;
-    }
-    // ドメイン外への遷移は制御しない
-    const element = target as HTMLAnchorElement;
-    if (element.href.indexOf(document.domain) === -1) {
-      return;
-    }
-
-    event.preventDefault();
-    window.history.pushState({}, '惨劇RoopeR online tool', element.href);
-    // elm に URLの変更を伝える
-    ports.changeUrl.send(element.href);
-  });
-
-  // ブラウザの戻るボタンを押したときの挙動を設定
-  window.addEventListener('popstate', (event) => { console.log(event); ports.changeUrl.send(window.location.href); });
-
-  // elm に 表示時のページURLを伝える
-  ports.changeUrl.send(window.location.href);
 };
 
 // 初期設定実行
---
+++ src/assets/js/utils/history.ts
@@ -0,0 +1,29 @@
+export const historyInit = (ports) => {
+  console.log('history init')
+  // histroy api 設定.遷移 を防ぐ。aタグは増減するので、親でバブリングしたイベントを受ける。
+  //  eslint-disable-next-line no-unused-expressions
+  document.querySelector('.rooper-container')?.addEventListener('click', (event) => {
+    // aタグ以外は処理をしない
+    const { target }: { target: any } = event;
+
+    if (target && target.tagName && target.tagName.toUpperCase() !== 'A') {
+      return;
+    }
+    // ドメイン外への遷移は制御しない
+    const element = target as HTMLAnchorElement;
+    if (element.href.indexOf(document.domain) === -1) {
+      return;
+    }
+
+    event.preventDefault();
+    window.history.pushState({}, '惨劇RoopeR online tool', element.href);
+    // elm に URLの変更を伝える
+    ports.changeUrl.send(element.href);
+  });
+
+  // ブラウザの戻るボタンを押したときの挙動を設定
+  window.addEventListener('popstate', (event) => { console.log(event); ports.changeUrl.send(window.location.href); });
+
+  // elm に 表示時のページURLを伝える
+  ports.changeUrl.send(window.location.href);
+};
